46265fef5071e40363f05c7759663d6708afc1f1
kenken64 <bunnyppl@hotmail.com>
2026-01-08 06:12:20 +0800

    fix: set coverage thresholds to 80%

--- peekachoo-admin/jest.config.js
+++ peekachoo-admin/jest.config.js
@@ -25,10 +25,10 @@ const customJestConfig = {
   coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   verbose: true,
